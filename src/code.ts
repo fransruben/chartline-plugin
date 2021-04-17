09aafbccbc3d84be38a1b675fa3944d330bbfcbc
fransruben <rubenvandenhout@gmail.com>
2021-04-17 19:25:32 +0200

    Add chartline to frame if frame selected

--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,5 @@
 figma.showUI(__html__)
-figma.ui.resize(350, 480);
+figma.ui.resize(350, 477);
 
 figma.ui.onmessage = msg => {
   if (msg.type === 'create-line') {
@@ -16,22 +16,26 @@ figma.ui.onmessage = msg => {
     strokes[0].color = color;
     ln.strokes = strokes;
 
-    figma.currentPage.appendChild(ln)
+    let selection = figma.currentPage.selection
+
+    if(selection.length > 0){
+      let frames = selection.filter(s => s.type === "FRAME") as FrameNode[]
+      if(frames.length > 0){
+        let f = frames[0]
+        ln.x = f.width/2 - ln.width/2
+        ln.y = f.height/2 - ln.height/2
+        f.appendChild(ln)
+      }
+      else {
+        figma.currentPage.appendChild(ln)
+      }
+    }
+  
+    // Add to page
     nodes.push(ln)
   
     figma.viewport.scrollAndZoomIntoView(nodes)
     figma.closePlugin()
-
-    // for (let i = 0; i < msg.count; i++) {
-    //   const rect = figma.createRectangle()
-    //   rect.x = i * 150
-    //   rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}]
-    //   figma.currentPage.appendChild(rect)
-    //   nodes.push(rect)
-    // }
-
-    // figma.currentPage.selection = nodes
-    // figma.viewport.scrollAndZoomIntoView(nodes)
   }
 }
 
