9b7e8fd11c0e330c6ed6849cee70798f183c9dec
fransruben <rubenvandenhout@gmail.com>
2022-11-11 16:04:18 +0100

    Stay open and add in center of viewport

--- src/code.ts
+++ src/code.ts
@@ -1,63 +1,69 @@
-figma.showUI(__html__)
+figma.showUI(__html__);
 figma.ui.resize(350, 477);
 
-figma.ui.onmessage = msg => {
-  if (msg.type === 'create-line') {
-    console.log(msg.type)
+figma.ui.onmessage = (msg) => {
+  if (msg.type === "create-line") {
+    console.log(msg.type);
 
-    const nodes = []
+    const nodes = [];
 
-    const ln = figma.createVector()
-    ln.vectorPaths = [{windingRule: "NONE", data: msg.parsedLine}]
-    
+    const ln = figma.createVector();
+    ln.vectorPaths = [{ windingRule: "NONE", data: msg.parsedLine }];
+
     // Set color of line
-    const color = {r:24/255, g:160/255, b:251/255 };
-    let strokes = clone(ln.strokes)
+    const color = { r: 24 / 255, g: 160 / 255, b: 251 / 255 };
+    let strokes = clone(ln.strokes);
     strokes[0].color = color;
     ln.strokes = strokes;
 
-    let selection = figma.currentPage.selection
-
-    if(selection.length > 0){
-      let frames = selection.filter(s => s.type === "FRAME") as FrameNode[]
-      if(frames.length > 0){
-        let f = frames[0]
-        ln.x = f.width/2 - ln.width/2
-        ln.y = f.height/2 - ln.height/2
-        f.appendChild(ln)
+    let selection = figma.currentPage.selection;
+    
+    if (selection.length > 0) {
+      // Put in center of selected frame
+      let frames = selection.filter((s) => s.type === "FRAME") as FrameNode[];
+      if (frames.length > 0) {
+        let f = frames[0];
+        ln.x = f.width / 2 - ln.width / 2;
+        ln.y = f.height / 2 - ln.height / 2;
+        f.appendChild(ln);
+      } else {
       }
-      else {
-        figma.currentPage.appendChild(ln)
-      }
+    } else {
+      // Put in center of viewport
+      ln.x = figma.viewport.center.x - ln.width / 2;
+      ln.y = figma.viewport.center.y - ln.height / 2;
+      figma.currentPage.appendChild(ln);
     }
-  
+
     // Add to page
-    nodes.push(ln)
-  
+    nodes.push(ln);
     figma.viewport.scrollAndZoomIntoView(nodes)
-    figma.closePlugin()
   }
-}
+};
 
 function clone(val) {
-  const type = typeof val
+  const type = typeof val;
   if (val === null) {
-    return null
-  } else if (type === 'undefined' || type === 'number' ||
-             type === 'string' || type === 'boolean') {
-    return val
-  } else if (type === 'object') {
+    return null;
+  } else if (
+    type === "undefined" ||
+    type === "number" ||
+    type === "string" ||
+    type === "boolean"
+  ) {
+    return val;
+  } else if (type === "object") {
     if (val instanceof Array) {
-      return val.map(x => clone(x))
+      return val.map((x) => clone(x));
     } else if (val instanceof Uint8Array) {
-      return new Uint8Array(val)
+      return new Uint8Array(val);
     } else {
-      let o = {}
+      let o = {};
       for (const key in val) {
-        o[key] = clone(val[key])
+        o[key] = clone(val[key]);
       }
-      return o
+      return o;
     }
   }
-  throw 'unknown'
+  throw "unknown";
 }
